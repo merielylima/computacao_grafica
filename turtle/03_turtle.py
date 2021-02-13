3f47bbcb684fcce8531e836cf9069a55f2b7a984
Meriely Lima <58087344+merielylima@users.noreply.github.com>
2021-02-13 14:45:58 -0300

    desenhos geométricos com turtle

---
+++ turtle/03_turtle.py
@@ -0,0 +1,38 @@
+from turtle import *
+
+speed('fast')
+pensize(5)
+
+for i in range(6):
+	pencolor('green')
+	setpos(0,0)
+	for i in range(4):
+		fd(100)
+		left(90)
+	right(60)
+right(45)	
+for i in range(6):
+	pencolor('blue')
+	setpos(0,0)
+	for i in range(4):
+		fd(100)
+		left(90)
+	right(60)
+right(45)	
+for i in range(6):
+	pencolor('red')
+	setpos(0,0)
+	for i in range(4):
+		fd(100)
+		left(90)
+	right(60)  
+right(45)	
+for i in range(6):
+	pencolor('yellow')
+	setpos(0,0)
+	for i in range(4):
+		fd(100)
+		left(90)
+	right(60)                                      
+
+done()
